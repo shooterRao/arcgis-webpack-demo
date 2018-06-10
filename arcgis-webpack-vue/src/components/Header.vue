d98f46cc0e64b5907f76eb6d7a09e0623abfe1fc
shooterRao <rjw0319@163.com>
2018-06-10 14:12:14 +0800

    arcgis-webpack-vue

---
+++ arcgis-webpack-vue/src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+<div class="header">
+  {{title}}
+</div>  
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data() {
+    return {
+      title : 'arcgis-webpack-vue'
+    }
+  }
+}
+</script>
+
+<style lang="less">
+.header {
+  width: 100%;
+  height: 100px;
+  line-height: 100px;
+  text-align: center;
+  background: rgb(61, 125, 219);
+  font-size: 30px;
+  color: aliceblue
+}
+</style>
+
